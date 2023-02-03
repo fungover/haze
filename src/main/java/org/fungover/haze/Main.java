b3563a952b44dec28c8159005285179e747c8853
Martin Blomberg <martin.blomberg@outlook.com>
2023-02-03 11:37:50 +0100

    Add code for opening ServerSocket
    Creates a server socket object in main method and accepts client connections.
    Some example code for reading and writing to the client is also included.

--- src/main/java/org/fungover/haze/Main.java
+++ src/main/java/org/fungover/haze/Main.java
@@ -1,7 +1,32 @@
 package org.fungover.haze;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        try (ServerSocket serverSocket = new ServerSocket(6379)) {
+            while (true) {
+                var client = serverSocket.accept();
+
+                BufferedReader input = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                System.out.println(input.readLine());
+                System.out.println(input.readLine());
+                System.out.println(input.readLine());
+                System.out.println(input.readLine());
+                System.out.println(input.readLine());
+                System.out.println(input.readLine());
+                System.out.println(input.readLine());
+
+                client.getOutputStream().write("+OK\r\n".getBytes());
+
+                client.close();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
