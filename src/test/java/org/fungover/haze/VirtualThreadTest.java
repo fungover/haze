5e54e24309c3944c22d522adaaf29aaac5f4d1e2
Oliver Säfström <safstrom.oliver@gmail.com>
2023-02-06 10:18:18 +0100

    Add addToDatabase Test

--- src/test/java/org/fungover/haze/VirtualThreadTest.java
+++ src/test/java/org/fungover/haze/VirtualThreadTest.java
@@ -1,8 +1,6 @@
 package org.fungover.haze;
 
 import org.junit.jupiter.api.Test;
-
-import java.util.Collections;
 import java.util.HashMap;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -12,17 +10,27 @@ class VirtualThreadTest {
 	HashMap<String, String> testDatabase = new HashMap<>();
 	VirtualThread virtualThread = new VirtualThread(testDatabase);
 
-
 	@Test
-	void ThreadStartingShouldReturnThreadNames() {
+	void addToDatabase() {
+		virtualThread.setDatabaseKey("testKey");
+		virtualThread.setDatabaseValue("testValue");
+		virtualThread.run();
+
+		assertEquals("testValue", virtualThread.getDatabase().get("testKey"));
+
+	}
+	@Test
+	void addToDatabaseWithOwnThread() {
 
 		virtualThread.startNewVirtualThread("1", "1", testDatabase);
 		virtualThread.startNewVirtualThread("2", "2", testDatabase);
-		System.out.println(testDatabase);
+
+
 		assertEquals("1", testDatabase.get("1"));
 
 
 	}
+
 
 
 }
