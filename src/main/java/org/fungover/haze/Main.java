635358057697f35b6da25b2659a9440cb87e6c27
Oliver Säfström <safstrom.oliver@gmail.com>
2023-02-06 21:00:59 +0100

    Add VirtualThreads

--- src/main/java/org/fungover/haze/Main.java
+++ src/main/java/org/fungover/haze/Main.java
@@ -4,29 +4,44 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Main {
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        try (ServerSocket serverSocket = new ServerSocket(6379)) {
-            while (true) {
-                var client = serverSocket.accept();
+		try (ServerSocket serverSocket = new ServerSocket(6379)) {
+			while (true) {
+				var client = serverSocket.accept();
 
-                BufferedReader input = new BufferedReader(new InputStreamReader(client.getInputStream()));
-                System.out.println(input.readLine());
-                System.out.println(input.readLine());
-                System.out.println(input.readLine());
-                System.out.println(input.readLine());
-                System.out.println(input.readLine());
-                System.out.println(input.readLine());
-                System.out.println(input.readLine());
+				Runnable newThread = () -> {
+					try {
+						BufferedReader input = new BufferedReader(new InputStreamReader(client.getInputStream()));
+						System.out.println(input.readLine());
+						System.out.println(input.readLine());
+						System.out.println(input.readLine());
+						System.out.println(input.readLine());
+						System.out.println(input.readLine());
 
-                client.getOutputStream().write("+OK\r\n".getBytes());
 
-                client.close();
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
+						System.out.println("ThreadID " + Thread.currentThread().threadId());  // Only for Debug
+						System.out.println("Is virtual Thread " + Thread.currentThread().isVirtual()); // Only for Debug
+
+
+						client.getOutputStream().write("+OK\r\n".getBytes());
+
+						client.close();
+
+					} catch (IOException e) {
+						throw new RuntimeException(e);
+					}
+				};
+				Thread newTHread = Thread.startVirtualThread(newThread);
+
+			}
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+
+
+		}
+	}
 }
