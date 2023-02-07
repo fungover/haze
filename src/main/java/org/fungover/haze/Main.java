c0f4800727d4824dbb2fe38b33a1ce0e3c66c9e3
Oliver Säfström <56272436+safstromo@users.noreply.github.com>
2023-02-07 09:37:10 +0100

    Update Main.java
    Cleanup

--- src/main/java/org/fungover/haze/Main.java
+++ src/main/java/org/fungover/haze/Main.java
@@ -45,7 +45,7 @@ public class Main {
 						throw new RuntimeException(e);
 					}
 				};
-				Thread newTHread = Thread.startVirtualThread(newThread);
+				Thread.startVirtualThread(newThread);
 
 			}
 		} catch (IOException e) {
