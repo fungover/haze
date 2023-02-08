b0b4a9f877cd676d0c1ae5de2aca8b83d6e21efd
MikaelEdwartz <Mikael.edwartz@gmail.com>
2023-02-08 11:17:14 +0100

    Add ReentrantLock to each method manipulating the map

--- src/main/java/org/fungover/haze/HazeDatabase.java
+++ src/main/java/org/fungover/haze/HazeDatabase.java
@@ -2,37 +2,70 @@ package org.fungover.haze;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class HazeDatabase {
 
 	private static Map<String, String> database;
+	private static Lock lock;
 
 	public HazeDatabase() {
 		this.database = new HashMap<>();
+		this.lock = new ReentrantLock();
 	}
 
+
 	private String set() {
-		//add code for setting value when when key exists
+		lock.lock();
+		try {
+			//add code for setting value when when key exists
+		} finally {
+			lock.unlock();
+		}
 		return "";
 	}
 
 	private String get() {
-		//return value when key is passed
+		lock.lock();
+		try {
+			//return value when key is passed
+		} finally {
+			lock.unlock();
+		}
 		return "";
 	}
 
 	private String delete() {
-		//remove key when it is passed, ignores if there is no key
+		lock.lock();
+		try {
+			//remove key when it is passed, ignores if there is no key
+		} finally {
+			lock.unlock();
+
+		}
 		return "";
 	}
 
 	private String exists() {
-		//gets key as parameter and returns an integer representing how many keys exists
+		lock.lock();
+		try {
+			//gets key as parameter and returns an integer representing how many keys exists
+		} finally {
+			lock.unlock();
+
+		}
 		return "";
 	}
 
 	private String setNX() {
-		//sets value if key does not exists, if there is a key this operation is ignored.
+		lock.lock();
+		try {
+			//sets value if key does not exists, if there is a key this operation is ignored.
+		} finally {
+			lock.unlock();
+
+		}
 		return "";
 	}
 }
