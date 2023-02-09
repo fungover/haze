925d083b976756a888df698f546fc4618d65517a
Henrik-Bengtsson <112540515+Henrik-Bengtsson@users.noreply.github.com>
2023-02-09 16:08:56 +0100

    Add execute command method.

--- src/main/java/org/fungover/haze/Main.java
+++ src/main/java/org/fungover/haze/Main.java
@@ -4,11 +4,15 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Main {
 	public static void main(String[] args) {
+
+		HazeDatabase hazeDatabase = new HazeDatabase();
 
 		try (ServerSocket serverSocket = new ServerSocket(6379)) {
 			while (true) {
@@ -24,7 +28,9 @@ public class Main {
                         String firstReading = input.readLine();
                         readInputStream(input, inputList, firstReading);
 
-                        inputList.forEach(System.out::println); // For checking incoming message
+				        executeCommand(hazeDatabase, client, inputList);
+
+				        inputList.forEach(System.out::println); // For checking incoming message
 
 						printThreadDebug();
 
@@ -45,14 +51,36 @@ public class Main {
 		System.out.println("ThreadID " + Thread.currentThread().threadId());  // Only for Debug
 		System.out.println("Is virtual Thread " + Thread.currentThread().isVirtual()); // Only for Debug
 	}
-    private static void readInputStream(BufferedReader input, List<String> inputList, String firstReading) throws IOException {
-        int size;
-        if (firstReading.startsWith("*")) {
-            size = Integer.parseInt(firstReading.substring(1)) * 2;
-            for (int i = 0; i < size; i++) {
-                inputList.add(input.readLine());
-            }
-        } else
-            inputList.add(firstReading);
-    }
+
+	private static void executeCommand(HazeDatabase hazeDatabase, Socket client, List<String> inputList) throws IOException {
+		String command = inputList.get(0);
+		String key = inputList.get(1);
+		String value = getValueIfExist(inputList);
+
+		switch (command) {
+			case "SETNX" -> client.getOutputStream().write(hazeDatabase.setNX(key, value).getBytes());
+			default -> client.getOutputStream().write("-ERR unknown command\r\n".getBytes());
+		}
+	}
+
+	private static String getValueIfExist(List<String> inputList) {
+		if (inputList.size() == 3)
+			return inputList.get(2);
+		return "";
+	}
+
+	private static void readInputStream(BufferedReader input, List<String> inputList, String firstReading) throws IOException {
+		int size;
+		if (firstReading.startsWith("*")) {
+			size = Integer.parseInt(firstReading.substring(1)) * 2;
+			for (int i = 0; i < size; i++) {
+				String temp = input.readLine();
+				if (!temp.contains("$"))
+					inputList.add(temp);
+			}
+		} else {
+			String[] seperated = firstReading.split("\\s");
+			inputList.addAll(Arrays.asList(seperated));
+		}
+	}
 }
