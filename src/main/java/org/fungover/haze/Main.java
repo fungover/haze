32698b8537aae5190000337812db0a0fd73fe0d0
VenuModi <venu.modi@iths.se>
2023-02-11 18:41:47 +0100

    Add log4j2 feature
    Added log4j2 dependency in pom.xml.
    Created a log4j2 class with seperate methods for calling info, warn,
    error & debug that can be called in other classes.
    For now we wont save log messages in a file. But if we want to activate
    it, we can just uncomment the lines in log4j2.xml under the resources
    folder.
    Changed class name log4j2 to Log4j2.
    Rebase main into branch

--- src/main/java/org/fungover/haze/Main.java
+++ src/main/java/org/fungover/haze/Main.java
@@ -3,6 +3,7 @@ package org.fungover.haze;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
