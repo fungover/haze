ab9b8e15d4748db18031a8b8b8d15cabab325380
Simon T <simtho0624@gmail.com>
2023-02-13 10:17:11 +0100

    Add Create a File

--- src/main/java/org/fungover/haze/SaveFile.java
+++ src/main/java/org/fungover/haze/SaveFile.java
@@ -1,4 +1,34 @@
 package org.fungover.haze;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class SaveFile {
+    public static void main(String[] args) {
+        createFile();
+    }
+    public static void createFile() {
+        String FormatFile = ".txt";
+        String NameFile = "test";
+        String homeFolder = System.getProperty("user.home");
+        Path filePath = Path.of(homeFolder,"IdeaProjects","haze",NameFile+FormatFile);
+
+        try {
+            System.out.println(Files.exists(filePath));
+            if (!Files.exists(filePath))
+            {
+            System.out.println("The file is now Created.");
+            Files.createFile(filePath);
+            }else if (Files.exists(filePath)){
+                System.out.println("The file exists.");
+            }
+
+
+        }catch (IOException e){
+            System.out.println("restart");
+        }
+
+    }
+
 }
