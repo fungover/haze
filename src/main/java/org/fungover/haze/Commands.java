4f6a01d8f1f3b6f67efc30ef93a5f6e185cc8bcd
Christofer Nyström <christofer.nystrom@iths.se>
2023-02-13 11:31:23 +0100

    Add command switch and command enum

---
+++ src/main/java/org/fungover/haze/Commands.java
@@ -0,0 +1,25 @@
+package org.fungover.haze;
+
+public class Commands {
+    public void command(Command cmd) {
+        switch (cmd) {
+            case SET -> setCommand();
+            case GET -> getCommand();
+            case DEL -> delCommand();
+            case PING -> pingCommand();
+            default -> System.out.println("Invalid Command");
+        }
+    }
+
+    private void setCommand() {
+    }
+
+    private void getCommand() {
+    }
+
+    private void delCommand() {
+    }
+
+    private void pingCommand() {
+    }
+}
