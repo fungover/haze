6bcd1cd11ca973b84efbfed991b71ca969bb9319
jLereback <112405931+jLereback@users.noreply.github.com>
2023-02-14 22:23:15 +0100

    Add non-case sensitive command with test

--- src/test/java/org/fungover/haze/MainTest.java
+++ src/test/java/org/fungover/haze/MainTest.java
@@ -18,4 +18,10 @@ class MainTest {
     void callingExecuteCommandWithInvalidInputStringReturnsErrorMessage() {
         assertThat(Main.executeCommand(database, List.of(""))).isEqualTo("-ERR unknown command\r\n");
     }
+
+    @Test
+    void executeCommandCanHandleCommandsInBothUpperAndLowerCase() {
+        Main.executeCommand(database, List.of("sEtNx", "1", "This is a value"));
+        assertThat(Main.executeCommand(database, List.of("sEtNx", "1", "This is also a value"))).isEqualTo(":0\r\n");
+    }
 }
