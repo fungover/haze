67dd9691259f42e903c48bd8c102d1bc169787fc
RobertMili <robertmilicevic3869@gmail.com>
2023-02-14 09:35:14 +0100

    1. Change the name of class and test class 2. More test were written

---
+++ src/main/java/org/fungover/haze/InMemoryRedisServer.java
@@ -0,0 +1,31 @@
+package org.fungover.haze;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class InMemoryRedisServer {
+    private Map<String, String> database;
+
+    public InMemoryRedisServer() {
+        database = new HashMap<>();
+    }
+
+    public String set(String key, String value) {
+        database.put(key, value);
+
+        return "+OK\r\n";
+    }
+
+    public String get(String key) {
+        if (database.containsKey(key)) {
+            return getString(key);
+        } else {
+            return "$-1\r\n";
+        }
+    }
+
+    public String getString(String key) {
+        return "$" + database.get(key).length() + "\r\n" + database.get(key) + "\r\n";
+    }
+
+}
---
+++ src/test/java/org/fungover/haze/InMemoryRedisServerTest.java
@@ -0,0 +1,59 @@
+package org.fungover.haze;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InMemoryRedisServerTest {
+    InMemoryRedisServer redisServer = new InMemoryRedisServer();
+
+
+    @Test
+    public void testSetWithValidKeyValuePair() {
+        String result = redisServer.set("key", "value");
+        assertEquals("+OK\r\n", result);
+    }
+
+    @Test
+    public void testSetWithNullValue() {
+        String result = redisServer.set("key", null);
+        assertEquals("+OK\r\n", result);
+    }
+
+    @Test
+    public void testGetWithValidKey() {
+        redisServer.set("key", "value");
+        String result = redisServer.get("key");
+        assertEquals("$5\r\nvalue\r\n", result);
+    }
+
+    @Test
+    public void testGetWithInvalidKey() {
+        String result = redisServer.get("invalidKey");
+        assertEquals("$-1\r\n", result);
+    }
+
+    @Test
+    public void testGetWithNullKey() {
+        String result = redisServer.get(null);
+        assertEquals("$-1\r\n", result);
+    }
+
+    @Test
+    public void testGetStringWithValidKey() {
+        redisServer.set("key", "value");
+        String result = redisServer.getString("key");
+        assertEquals("$5\r\nvalue\r\n", result);
+    }
+
+    @Test
+    public void testGetStringWithNullKey() {
+        try {
+            redisServer.getString(null);
+            fail();
+        } catch (NullPointerException npe) {
+
+        }
+    }
+
+}
