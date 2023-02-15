94ab1d393bedea8bbc9506356b9cb0bebc3f3c4d
Saher Ismail <112554103+rehasliamsi@users.noreply.github.com>
2023-02-15 12:43:45 +0100

    Update test with List.of
    Co-authored-by: Julia Lerebäck Corell <112405931+jLereback@users.noreply.github.com>

--- src/test/java/org/fungover/haze/HazeDatabaseTest.java
+++ src/test/java/org/fungover/haze/HazeDatabaseTest.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 import java.util.Collections;
 
+import java.util.Collections;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -53,7 +56,7 @@ class HazeDatabaseTest {
     void testSettingOneKeyInDatabaseMakesExistsFunctionReturnOneInstanceOfKeyExistingInTheDatabase() {
         testDatabase.setNX("name", "saher");
 
-        assertThat(testDatabase.exists("name")).isEqualTo(":1\r\n");
+        assertThat(testDatabase.exists(List.of("name"))).isEqualTo(":1\r\n");
     }
 
     @Test
