b0097b4ebb1d2ddf25072f36d3fe20ab613892a5
Christofer Nyström <christofer.nystrom@iths.se>
2023-02-15 10:38:28 +0100

    Add lock and unlock and fixes method names

--- src/main/java/org/fungover/haze/HazeList.java
+++ src/main/java/org/fungover/haze/HazeList.java
@@ -1,134 +1,194 @@
 package org.fungover.haze;
 
 import java.util.*;
+import java.util.concurrent.locks.*;
 
 public class HazeList {
 
     final Map<String, List<String>> database;
+    final ReentrantLock lock;
 
     public HazeList() {
         this.database = new HashMap<>();
+        this.lock = new ReentrantLock();
     }
 
-    public String LPUSH(String key, String... values) {
-        List<String> list = database.computeIfAbsent(key, k -> new ArrayList<>());
-        List<String> tempList = Arrays.asList(values);
-        Collections.reverse(tempList);
-        list.addAll(0, tempList);
-        return ":" + list.size() + "\r\n";
-    }
-
-    public String RPUSH(String key, String... values) {
-        List<String> list = database.computeIfAbsent(key, k -> new ArrayList<>());
-        list.addAll(Arrays.asList(values));
-        return ":" + list.size() + "\r\n";
-    }
-
-    //OVERLOAD
-    public String LPOP(String key) {
-        if (!database.containsKey(key) || database.get(key).isEmpty())
-            return "$5\r\n(nil)\r\n";
-        int lengthOfValue = database.get(key).get(0).length();
-        return "$" + lengthOfValue + "\r\n" +database.get(key).remove(0) + "\r\n";
-    }
-
-    //OVERLOAD
-    public String LPOP(String key, int count) {
-        if (!database.containsKey(key) || database.get(key).isEmpty())
-            return "$5\r\n(nil)\r\n";
-
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("*").append(count).append("\r\n");
-
-        for (int i = 0; i < count; i++) {
-            if (database.get(key).isEmpty())
-                return stringBuilder.toString();
-
-            String value = database.get(key).remove(0);
-            stringBuilder.append("$").append(value.length()).append("\r\n").append(value).append("\r\n");
-        }
-        return stringBuilder.toString();
-    }
-
-    //OVERLOAD
-    public String RPOP(String key) {
-        if (!database.containsKey(key) || database.get(key).isEmpty())
-            return "$5\r\n(nil)\r\n";
-        int lengthOfValue = database.get(key).get(0).length();
-        int lastIndex = database.get(key).size()-1;
-        return "$" + lengthOfValue + "\r\n" + database.get(key).remove(lastIndex) + "\r\n";
-    }
-
-    //OVERLOAD
-    public String RPOP(String key, int count) {
-        if (!database.containsKey(key) || database.get(key).isEmpty())
-            return "$5\r\n(nil)\r\n";
-
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("*").append(count).append("\r\n");
-
-        for (int i = 0; i < count; i++) {
-            if (database.get(key).isEmpty())
-                return stringBuilder.toString();
-
-            int lastIndex = database.get(key).size()-1;
-            String value = database.get(key).remove(lastIndex);
-            stringBuilder.append("$").append(value.length()).append("\r\n").append(value).append("\r\n");
-        }
-        return stringBuilder.toString();
-    }
-
-    public String LLEN(String key) {
-        if (database.get(key) == null)
-            return ":0\r\n";
-        return ":" + database.get(key).size() + "\r\n";
-    }
-
-
-    public String LMOVE(String source, String destination, String whereFrom, String whereTo) {
-
-        if (database.get(source) == null || database.get(destination) == null)
-            return "-One or both keys do not contain lists.\r\n";
-        else if (database.get(source).isEmpty())
-            return "-The source list is empty.\r\n";
-
-        String value;
-
-        if (whereFrom.equals("LEFT") && whereTo.equals("LEFT")) {
-            value = database.get(source).remove(0);
-            database.get(destination).add(0, value);
-        }
-        else if (whereFrom.equals("LEFT") && whereTo.equals("RIGHT")) {
-            value = database.get(source).remove(0);
-            database.get(destination).add(value);
-        }
-        else if (whereFrom.equals("RIGHT") && whereTo.equals("LEFT")) {
-            value = database.get(source).remove(database.get(source).size() - 1);
-            database.get(destination).add(0, value);
-        }
-        else if (whereFrom.equals("RIGHT") && whereTo.equals("RIGHT")) {
-            value = database.get(source).remove(database.get(source).size() - 1);
-            database.get(destination).add(value);
-        }
-        else
-            return "-Invalid input for FROM and WHERE.\r\n\"";
-
-        return "$" + value.length() + "\r\n" + value + "\r\n";
-    }
-
-
-    public String LTRIM(String key, int start, int stop) {
-        if (!database.containsKey(key))
-            return("-The key is not present in the database.\r\n");
-
+    public String lPush(String key, String... values) {
+        lock.lock();
         try {
-            database.put(key, new ArrayList<>(database.get(key).subList(start, stop+1)));
-            return "+OK\r\n";
+            List<String> list = database.computeIfAbsent(key, k -> new ArrayList<>());
+            List<String> tempList = Arrays.asList(values);
+            Collections.reverse(tempList);
+            list.addAll(0, tempList);
+            return ":" + list.size() + "\r\n";
         }
-        catch (IndexOutOfBoundsException e) {
-            return "-The inputs are outside the range of the list.\r\n";
+        finally {
+            lock.unlock();
         }
     }
+
+    public String rPush(String key, String... values) {
+        lock.lock();
+        try {
+            List<String> list = database.computeIfAbsent(key, k -> new ArrayList<>());
+            list.addAll(Arrays.asList(values));
+            return ":" + list.size() + "\r\n";
+        }
+        finally {
+            lock.unlock();
+        }
+    }
+
+    //OVERLOAD
+    public String lPop(String key) {
+        lock.lock();
+        try {
+            if (!database.containsKey(key) || database.get(key).isEmpty())
+                return "$5\r\n(nil)\r\n";
+            int lengthOfValue = database.get(key).get(0).length();
+            return "$" + lengthOfValue + "\r\n" +database.get(key).remove(0) + "\r\n";
+        }
+        finally {
+            lock.unlock();
+        }
+    }
+
+    //OVERLOAD
+    public String lPop(String key, int count) {
+        lock.lock();
+        try {
+            if (!database.containsKey(key) || database.get(key).isEmpty())
+                return "$5\r\n(nil)\r\n";
+
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.append("*").append(count).append("\r\n");
+
+            for (int i = 0; i < count; i++) {
+                if (database.get(key).isEmpty())
+                    return stringBuilder.toString();
+
+                String value = database.get(key).remove(0);
+                stringBuilder.append("$").append(value.length()).append("\r\n").append(value).append("\r\n");
+            }
+            return stringBuilder.toString();
+        }
+        finally {
+            lock.unlock();
+        }
+    }
+
+    //OVERLOAD
+    public String rPop(String key) {
+        lock.lock();
+        try {
+            if (!database.containsKey(key) || database.get(key).isEmpty())
+                return "$5\r\n(nil)\r\n";
+            int lengthOfValue = database.get(key).get(0).length();
+            int lastIndex = database.get(key).size()-1;
+            return "$" + lengthOfValue + "\r\n" + database.get(key).remove(lastIndex) + "\r\n";
+        }
+        finally {
+            lock.unlock();
+        }
+    }
+
+
+    //OVERLOAD
+    public String rPop(String key, int count) {
+        lock.lock();
+        try {
+            if (!database.containsKey(key) || database.get(key).isEmpty())
+                return "$5\r\n(nil)\r\n";
+
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.append("*").append(count).append("\r\n");
+
+            for (int i = 0; i < count; i++) {
+                if (database.get(key).isEmpty())
+                    return stringBuilder.toString();
+
+                int lastIndex = database.get(key).size()-1;
+                String value = database.get(key).remove(lastIndex);
+                stringBuilder.append("$").append(value.length()).append("\r\n").append(value).append("\r\n");
+            }
+            return stringBuilder.toString();
+        }
+        finally {
+            lock.unlock();
+        }
+    }
+
+
+    public String lLen(String key) {
+        lock.lock();
+        try {
+            if (database.get(key) == null)
+                return ":0\r\n";
+            return ":" + database.get(key).size() + "\r\n";
+        }
+        finally {
+            lock.unlock();
+        }
+    }
+
+
+    public String lMove(String source, String destination, String whereFrom, String whereTo) {
+        lock.lock();
+        try {
+            if (database.get(source) == null || database.get(destination) == null)
+                return "-One or both keys do not contain lists.\r\n";
+            else if (database.get(source).isEmpty())
+                return "-The source list is empty.\r\n";
+
+            String value;
+
+            if (whereFrom.equals("LEFT") && whereTo.equals("LEFT")) {
+                value = database.get(source).remove(0);
+                database.get(destination).add(0, value);
+            }
+            else if (whereFrom.equals("LEFT") && whereTo.equals("RIGHT")) {
+                value = database.get(source).remove(0);
+                database.get(destination).add(value);
+            }
+            else if (whereFrom.equals("RIGHT") && whereTo.equals("LEFT")) {
+                value = database.get(source).remove(database.get(source).size() - 1);
+                database.get(destination).add(0, value);
+            }
+            else if (whereFrom.equals("RIGHT") && whereTo.equals("RIGHT")) {
+                value = database.get(source).remove(database.get(source).size() - 1);
+                database.get(destination).add(value);
+            }
+            else
+                return "-Invalid input for FROM and WHERE.\r\n";
+
+            return "$" + value.length() + "\r\n" + value + "\r\n";
+        }
+        finally {
+            lock.unlock();
+        }
+    }
+
+
+
+    public String lTrim(String key, int start, int stop) {
+        lock.lock();
+        try {
+            if (!database.containsKey(key))
+                return("-The key is not present in the database.\r\n");
+
+            try {
+                database.put(key, new ArrayList<>(database.get(key).subList(start, stop+1)));
+                return "+OK\r\n";
+            }
+            catch (IndexOutOfBoundsException e) {
+                return "-The inputs are outside the range of the list.\r\n";
+            }
+        }
+        finally {
+            lock.unlock();
+        }
+    }
+
 
     @Override
     public String toString() {
--- src/test/java/org/fungover/haze/HazeListTest.java
+++ src/test/java/org/fungover/haze/HazeListTest.java
@@ -11,96 +11,96 @@ class HazeListTest {
 
     @Test
     void assertThatLPUSHWithMiltipleValuesAddsInReverseOrder() {
-        hazeList.LPUSH("key1", "value1", "value2");
+        hazeList.lPush("key1", "value1", "value2");
         String indexZero = hazeList.database.get("key1").get(0);
         assertEquals("value2", indexZero);
     }
 
     @Test
     void assertThatLpushWithOneValueGetIndexZero() {
-        hazeList.LPUSH("key1", "value1", "value2");
-        hazeList.LPUSH("key1", "X");
+        hazeList.lPush("key1", "value1", "value2");
+        hazeList.lPush("key1", "X");
         String indexZero = hazeList.database.get("key1").get(0);
         assertEquals("X", indexZero);
     }
 
     @Test
     void assertThatLlenReturnsCorrectValueAddedByLpushAndRpush() {
-        hazeList.LPUSH("key1", "value1", "value2");
-        hazeList.RPUSH("key1", "value1", "value2");
-        assertEquals(":4\r\n", hazeList.LLEN("key1"));
+        hazeList.lPush("key1", "value1", "value2");
+        hazeList.rPush("key1", "value1", "value2");
+        assertEquals(":4\r\n", hazeList.lLen("key1"));
     }
 
     @Test
     void lpopShouldReturnNilStringWhenNoKeyIsPressent() {
-        String nilTest= hazeList.LPOP("keyThatDontExist");
+        String nilTest= hazeList.lPop("keyThatDontExist");
         assertEquals("$5\r\n(nil)\r\n", nilTest);
     }
 
     @Test
     void lPopShouldReturnCorrectValues() {
-        hazeList.RPUSH("key1", "value1", "value2");
+        hazeList.rPush("key1", "value1", "value2");
 
         String expected = "*2\r\n$6\r\nvalue1\r\n$6\r\nvalue2\r\n";
 
-        assertEquals(expected, hazeList.LPOP("key1", 2));
+        assertEquals(expected, hazeList.lPop("key1", 2));
     }
 
     @Test
     void rPopShouldReturnValue2RespString() {
-        hazeList.RPUSH("key1", "value1", "value2");
+        hazeList.rPush("key1", "value1", "value2");
 
         String expected = "$6\r\nvalue2\r\n";
-        assertEquals(expected, hazeList.RPOP("key1" ));
+        assertEquals(expected, hazeList.rPop("key1" ));
     }
 
     @Test
     void rPopShouldReturnValuesInReverseOrder() {
-        hazeList.RPUSH("key1", "value1", "value2");
+        hazeList.rPush("key1", "value1", "value2");
 
         String expected = "*2\r\n$6\r\nvalue2\r\n$6\r\nvalue1\r\n";
-        assertEquals(expected, hazeList.RPOP("key1", 2));
+        assertEquals(expected, hazeList.rPop("key1", 2));
     }
     @Test
     void lMoveShouldMoveVal1FromLeftToLeft(){
-        hazeList.RPUSH("key1", "val1", "val2");
-        hazeList.RPUSH("key2", "val3", "val4");
-        hazeList.LMOVE("key1", "key2", "LEFT", "LEFT");
+        hazeList.rPush("key1", "val1", "val2");
+        hazeList.rPush("key2", "val3", "val4");
+        hazeList.lMove("key1", "key2", "LEFT", "LEFT");
         String shouldBeVal1 = hazeList.database.get("key2").get(0);
         assertEquals(shouldBeVal1, "val1");
     }
 
     @Test
     void lMoveShouldMoveVal1FromLeftToRight(){
-        hazeList.RPUSH("key1", "val1", "val2");
-        hazeList.RPUSH("key2", "val3", "val4");
-        hazeList.LMOVE("key1", "key2", "LEFT", "RIGHT");
+        hazeList.rPush("key1", "val1", "val2");
+        hazeList.rPush("key2", "val3", "val4");
+        hazeList.lMove("key1", "key2", "LEFT", "RIGHT");
         String shouldBeVal1 = hazeList.database.get("key2").get(2);
         assertEquals(shouldBeVal1, "val1");
     }
 
     @Test
     void lMoveShouldRemoveValueWhenMoved(){
-        hazeList.RPUSH("key1", "val1", "val2");
-        hazeList.RPUSH("key2", "val3", "val4");
-        hazeList.LMOVE("key1", "key2", "RIGHT", "LEFT");
+        hazeList.rPush("key1", "val1", "val2");
+        hazeList.rPush("key2", "val3", "val4");
+        hazeList.lMove("key1", "key2", "RIGHT", "LEFT");
         int shouldBeSizeOne = hazeList.database.get("key1").size();
         assertEquals(shouldBeSizeOne, 1);
     }
 
     @Test
     void destinationShouldHaveCorrectSizeAfterLmove(){
-        hazeList.RPUSH("key1", "val1", "val2");
-        hazeList.RPUSH("key2", "val3", "val4");
-        hazeList.LMOVE("key1", "key2", "RIGHT", "RIGHT");
+        hazeList.rPush("key1", "val1", "val2");
+        hazeList.rPush("key2", "val3", "val4");
+        hazeList.lMove("key1", "key2", "RIGHT", "RIGHT");
         int shouldBeSizeThree = hazeList.database.get("key2").size();
         assertEquals(shouldBeSizeThree, 3);
     }
 
     @Test
     void correctValuesShouldStayAfterTrim() {
-        hazeList.RPUSH("key1", "val1", "val2", "val3", "val4", "val5");
-        hazeList.LTRIM("key1", 1,2);
+        hazeList.rPush("key1", "val1", "val2", "val3", "val4", "val5");
+        hazeList.lTrim("key1", 1,2);
         String valuesLeft = "[val2, val3]";
 
         assertEquals(valuesLeft, hazeList.database.get("key1").toString());
@@ -108,8 +108,8 @@ class HazeListTest {
 
     @Test
     void lTrimShouldReturnErrorCorrectErrorTextWhenInputsAreOutOfRange(){
-        hazeList.RPUSH("key1", "val1", "val2", "val3", "val4", "val5");
+        hazeList.rPush("key1", "val1", "val2", "val3", "val4", "val5");
         String correctErrorText = "-The inputs are outside the range of the list.\r\n";
-        assertEquals(correctErrorText, hazeList.LTRIM("key1", 2, 7));
+        assertEquals(correctErrorText, hazeList.lTrim("key1", 2, 7));
     }
 }
