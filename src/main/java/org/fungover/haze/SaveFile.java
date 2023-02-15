89df3f74788a1e1221064c096e02fd933a892196
Martin Blomberg <martin.blomberg@outlook.com>
2023-02-15 20:37:37 +0100

    Add logger to clean up SonarLint issues.

--- src/main/java/org/fungover/haze/SaveFile.java
+++ src/main/java/org/fungover/haze/SaveFile.java
@@ -1,9 +1,13 @@
 package org.fungover.haze;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
+import java.text.MessageFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Map;
@@ -14,6 +18,10 @@ import static java.nio.file.Files.createDirectory;
 
 public class SaveFile {
     private static Path saveFolder;
+    private static final Logger logger = LogManager.getLogger(SaveFile.class);
+
+    private SaveFile() {
+    }
 
     private static void createFolder() {
         String homeFolder = System.getProperty("user.home");
@@ -22,10 +30,10 @@ public class SaveFile {
         try {
             createDirectory(f);
             createDirectory(h);
+            saveFolder = h;
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            logger.error(MessageFormat.format("Failed creating save folder:{0}", e.getMessage()));
         }
-        saveFolder = h;
     }
 
     private static Path getPath() {
@@ -44,13 +52,13 @@ public class SaveFile {
         Path filePath = getPath();
         try {
             if (!Files.exists(filePath)) {
-                System.out.println("The file is now Created.");
                 Files.createFile(filePath);
+                logger.debug("Created new file");
             } else if (Files.exists(filePath)) {
-                System.out.println("The file exists.");
+                logger.debug("File already exists");
             }
         } catch (IOException e) {
-            System.out.println("Restart 2.0");
+            logger.error(MessageFormat.format("Error creating save file: {0}", filePath));
         }
     }
 
@@ -59,13 +67,12 @@ public class SaveFile {
         String onError = "-Error 'message'\r\n";
         createFile();
         String convertMapToString = keyValues.entrySet().stream().map(e -> e.getKey() + "\n" + e.getValue() + "\n").collect(Collectors.joining());
-        System.out.println(convertMapToString);
         try {
             Files.writeString(getPath(), convertMapToString, StandardOpenOption.APPEND);
-            System.out.println(onSuccess);
+            logger.debug("Saved database to file");
             return onSuccess;
         } catch (IOException e) {
-            System.out.println(onError);
+            logger.error(MessageFormat.format("Failed to save database to file: {0}", e.getMessage()));
             return onError;
         }
     }
