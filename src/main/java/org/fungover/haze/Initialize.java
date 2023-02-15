e1e0219a65bc5d6cac2702758c658ac6aeaae216
Oliver Säfström <safstrom.oliver@gmail.com>
2023-02-15 11:34:52 +0100

    Add getPassword

--- src/main/java/org/fungover/haze/Initialize.java
+++ src/main/java/org/fungover/haze/Initialize.java
@@ -5,31 +5,42 @@ import java.util.Map;
 
 public class Initialize {
 
-	private static final Map<String, String> cliOptions = new HashMap<>();
+    private static final Map<String, String> cliOptions = new HashMap<>();
 
 
-	public void importCliOptions(String[] args) {
+    public void importCliOptions(String[] args) {
 
-		for (int i = 0; i < args.length; i++) {
-			if (i + 1 == args.length)
-				break;
-			cliOptions.put(args[i], args[i + 1]);
-		}
-	}
+        for (int i = 0; i < args.length; i++) {
+            if (i + 1 == args.length)
+                break;
+            cliOptions.put(args[i], args[i + 1]);
+        }
+    }
 
-	public int getPort() {
+    public int getPort() {
 
-		if (cliOptions.containsKey("-p")) {
-			return Integer.parseInt(cliOptions.get("-p"));
-		} else if (cliOptions.containsKey("--port")) {
-			return Integer.parseInt(cliOptions.get("--port"));
-		} else if ((System.getenv("HAZE_PORT") != null)) {
-			return Integer.parseInt(System.getenv("HAZE_PORT"));
-		} else return 6379;
-	}
+        if (cliOptions.containsKey("-p")) {
+            return Integer.parseInt(cliOptions.get("-p"));
+        } else if (cliOptions.containsKey("--port")) {
+            return Integer.parseInt(cliOptions.get("--port"));
+        } else if ((System.getenv("HAZE_PORT") != null)) {
+            return Integer.parseInt(System.getenv("HAZE_PORT"));
+        } else return 6379;
+    }
 
-	public void clearCliOptions() {
-		cliOptions.clear();
-	}
+    public String getPassword() {
+
+        if (cliOptions.containsKey("-pw")) {
+            return cliOptions.get("-pw");
+        } else if (cliOptions.containsKey("--password")) {
+            return cliOptions.get("--password");
+        } else if ((System.getenv("HAZE_PASSWORD") != null)) {
+            return System.getenv("HAZE_PASSWORD");
+        } else return null;
+    }
+
+    public void clearCliOptions() {
+        cliOptions.clear();
+    }
 
 }
