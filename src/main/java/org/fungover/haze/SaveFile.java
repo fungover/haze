78a24b0e36a968936d4556fe50e4c6aa4d025748
jLereback <112405931+jLereback@users.noreply.github.com>
2023-02-16 09:50:26 +0100

    Remove unused import
    Fix formatting

--- src/main/java/org/fungover/haze/SaveFile.java
+++ src/main/java/org/fungover/haze/SaveFile.java
@@ -3,7 +3,6 @@ package org.fungover.haze;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
