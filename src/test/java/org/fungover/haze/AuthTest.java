b26ec67da223d226ae12764074bd131bf674972f
Oliver Säfström <safstrom.oliver@gmail.com>
2023-02-16 14:37:38 +0100

    Remove unused imports

--- src/test/java/org/fungover/haze/AuthTest.java
+++ src/test/java/org/fungover/haze/AuthTest.java
@@ -2,18 +2,11 @@ package org.fungover.haze;
 
 
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 
-
-import java.io.IOException;
-import java.io.OutputStream;
 import java.net.Socket;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.mockito.Mockito.*;
 
 
 class AuthTest {
