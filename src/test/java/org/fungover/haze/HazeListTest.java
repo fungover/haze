80f1adb05c2c2b2d0e6225834e2566e5624cbdcb
Cristoffer matlak <cmatlak@hotmail.com>
2024-02-07 14:02:01 +0100

    Add test methods for LSET method
    Co-authored-by: robinalfengard <robin.alfengard@hotmail.com>

--- src/test/java/org/fungover/haze/HazeListTest.java
+++ src/test/java/org/fungover/haze/HazeListTest.java
@@ -1,9 +1,9 @@
 package org.fungover.haze;
 import org.junit.jupiter.api.Test;
 import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class HazeListTest {
 
