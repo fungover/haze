47d42fb76251d1a82e7e9b6727ee7a06d97173e3
Cristoffer matlak <cmatlak@hotmail.com>
2024-02-07 22:42:07 +0100

    Add more unit test on hazelist

--- src/test/java/org/fungover/haze/integration/HazeIT.java
+++ src/test/java/org/fungover/haze/integration/HazeIT.java
@@ -89,6 +89,15 @@ class HazeIT {
         pool.del("test");
         assertThat(pool.exists("right")).isFalse();
     }
+        @Test
+    void listLset(){
+            pool.set("test","OK");
+            assertThat(pool.lset("test", 0, "hej")).isEqualTo("OK");
+            assertThat(pool.exists("test")).isTrue();
+            pool.del("test");
+    }
+
+
 
     @Test
     void lindexReturnCorrectIndex() {
