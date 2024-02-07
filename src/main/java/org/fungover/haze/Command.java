c831d3e52ea8368ac23c79b11b857e58a6180757
Cristoffer matlak <cmatlak@hotmail.com>
2024-02-07 11:31:24 +0100

    Added LSET method,update switch and enum class

--- src/main/java/org/fungover/haze/Command.java
+++ src/main/java/org/fungover/haze/Command.java
@@ -1,9 +1,10 @@
 package org.fungover.haze;
 
 public enum Command {
+
     SET, GET, DEL, PING, SETNX, EXISTS, SAVE, RPUSH, LPUSH, LPOP, RPOP, LLEN, LMOVE, LTRIM, AUTH, LINDEX, INCR, DECR,LSET
 
-    
+
 
 
 
