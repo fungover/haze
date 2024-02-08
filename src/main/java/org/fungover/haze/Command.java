3b2ab236bd2cf44bb75e1e9df697339afd8d4ed9
Cristoffer matlak <cmatlak@hotmail.com>
2024-02-08 15:31:59 +0100

    fixed an error

--- src/main/java/org/fungover/haze/Command.java
+++ src/main/java/org/fungover/haze/Command.java
@@ -2,7 +2,8 @@ package org.fungover.haze;
 
 public enum Command {
 
-    
 
-    SET, GET, DEL, PING, SETNX, EXISTS, SAVE, RPUSH, LPUSH, LPOP, RPOP, LLEN, LMOVE, LTRIM, AUTH, INCR, DECR,LSET
+    SET, GET, DEL, PING, SETNX, EXISTS, SAVE, RPUSH, LPUSH, LPOP, RPOP, LLEN, LMOVE, LTRIM, AUTH, INCR, DECR, LSET
+
+}
 
