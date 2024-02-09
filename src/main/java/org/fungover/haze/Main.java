39a95b0241d9ef224279060f61523f6781690aa4
Cristoffer matlak <cmatlak@hotmail.com>
2024-02-09 14:17:38 +0100

    fixed an error in command

--- src/main/java/org/fungover/haze/Main.java
+++ src/main/java/org/fungover/haze/Main.java
@@ -12,7 +12,7 @@ import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
+
 
 public class Main {
     static boolean serverOpen = true;
