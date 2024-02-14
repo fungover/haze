36e66d62cf0b127c610db2103d276c98f7e1bc24
Cristoffer matlak <cmatlak@hotmail.com>
2024-02-14 07:31:11 +0100

    deleted import

--- src/test/java/org/fungover/haze/MainTest.java
+++ src/test/java/org/fungover/haze/MainTest.java
@@ -21,7 +21,6 @@ import java.util.ArrayList;
 import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.fungover.haze.Main.printThreadDebug;
-import static org.junit.Assert.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
