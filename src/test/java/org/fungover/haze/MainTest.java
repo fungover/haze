d594ddbc3f1e763d528046cd832b231abb1fb45c
Cristoffer matlak <cmatlak@hotmail.com>
2024-02-14 06:24:01 +0100

    delete imports

--- src/test/java/org/fungover/haze/MainTest.java
+++ src/test/java/org/fungover/haze/MainTest.java
@@ -2,7 +2,7 @@ package org.fungover.haze;
 
 
 
-import org.junit.jupiter.api.DisplayName;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
